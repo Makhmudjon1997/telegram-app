46f51c1eadcd13017394b1343e03057a5bb911aa
Adham2023 <adham.muhammadjonov1923@gmail.com>
2021-08-29 23:50:48 +0500

    ASDASD

--- src/layouts/main/components/Main/index.vue
+++ src/layouts/main/components/Main/index.vue
@@ -16,8 +16,8 @@ import Header from "./Header";
 import Message from "./Message";
 import Textarea from "./Textarea";
 import { onMounted } from "vue";
-import { useStore } from 'vuex'
-
+import { useStore } from "vuex";
+import api from "@/utils/mtproto3";
 export default {
   components: {
     Header,
@@ -28,9 +28,51 @@ export default {
     const store = useStore();
 
     onMounted(async () => {
-      await store.dispatch('GET_USER')
-      await store.dispatch('GET_DAILOGS')
-      await store.dispatch('GET_DIALOG_FILTERS')
+      await store.dispatch("GET_USER");
+      await store.dispatch("GET_DAILOGS");
+      await store.dispatch("GET_DIALOG_FILTERS")
+      
+      // (async () => {
+        const resolvedPeer = await api.call("contacts.resolveUsername", {
+          username: "alisher_khamraev_rustamugli"
+          // phone: '+998330990001'
+        });
+
+        console.log(resolvedPeer)
+
+        const user = resolvedPeer.users.find(
+          user => user.id === resolvedPeer.peer.user_id
+        );
+        console.log('User', user)
+        const inputPeer = {
+          _: "inputPeerUser",
+          user_id: user.id,
+          access_hash: user.access_hash
+        };
+
+        const LIMIT_COUNT = 40;
+        const allMessages = [];
+
+        // const firstHistoryResult = 
+        await api.call("messages.getHistory", {
+          peer: inputPeer,
+          limit: LIMIT_COUNT
+        });
+
+        // const historyCount = firstHistoryResult.count;
+
+        for (let offset = 0; offset < 1; offset += LIMIT_COUNT) {
+          const history = await api.call("messages.getHistory", {
+            peer: inputPeer,
+            add_offset: offset,
+            limit: LIMIT_COUNT
+          });
+
+          allMessages.push(...history.messages);
+        }
+
+        console.log("allMessages:", allMessages);
+      // })();
     });
   }
 };
